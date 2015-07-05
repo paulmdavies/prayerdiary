9b592966a0c93657ce72e04941632ddf492674cd
Paul Davies <paulmdavies@gmail.com>
2015-07-05 15:08:13 +0100

    Pulled out a main function in htmlTest.py

--- htmlTest.py
+++ htmlTest.py
@@ -10,7 +10,7 @@ class PrayerDay(object):
         self.date = date
         self.prayer_points = prayer_points
 
-with open("/tmp/jinjaTest.html", 'w') as f:
+def create_prayer_diary():
     with open("template.html", 'r') as template_file:
         template = "\n".join(template_file.readlines())
 
@@ -19,7 +19,14 @@ with open("/tmp/jinjaTest.html", 'w') as f:
             start_date="15th June",
             end_date="12th July",
             bible_verse = BibleVerse("Devote yourselves to prayer, being watchful and thankful", "Colossians 4:2"),
-            prayer_days = [PrayerDay("Today", ["Foo", "Bar", "Baz"]), PrayerDay("Tomorrow", ["Qux", "Quux", "Bippy"])]
+            prayer_days = [PrayerDay("Today", ["Foo", "Bar", "Baz"]), PrayerDay("Tomorrow", ["Qux", "Fooble", "Bippy"])]
         )
+        return rendered_template
 
-        f.write(rendered_template)
+def write_prayer_diary(diary):
+    with open("/tmp/jinjaTest.html", 'w') as f:
+        f.write(diary)
+
+if __name__ == "__main__":
+    diary = create_prayer_diary()
+    write_prayer_diary(diary)
