22a20e36fee7860cf1603b94ff6ffc2d759d0b38
Paul Davies <paulmdavies@gmail.com>
2015-07-11 10:31:40 +0100

    Add first pass prayer point class

---
+++ prayer_point.py
@@ -0,0 +1,5 @@
+class PrayerPoint(object):
+    def __init__(self, subject, detail, sub_points=[]):
+        self.subject = subject
+        self.detail = detail
+        self.sub_points = sub_points
